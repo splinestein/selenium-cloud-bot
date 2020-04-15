42b1cd36f169da20529978db8d9670d5195a6423
Anthon98 <anthon.kallankari@gmail.com>
2020-04-15 23:16:46 +0300

    release v0.01 alpha

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import re
+import time
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import os
 
 chrome_options = webdriver.ChromeOptions()
@@ -8,5 +14,41 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.get("https://www.google.com")
-print(driver.page_source)
+print("Active")
+
+def RUNMAIN():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://onlinesequencer.net/")
+    time.sleep(2)
+    button = driver.find_element_by_id("login_button")
+
+    ActionChains(driver).move_to_element(button).click(button).perform()
+    driver.find_element_by_id("username").send_keys("Void-BOT")
+    driver.find_element_by_id ("password").send_keys("kallan1914")
+    driver.find_element_by_id("login_button").click()
+
+    time.sleep(5)
+    driver.execute_script('''window.open("https://onlinesequencer.net/","_blank");''')
+    time.sleep(5)
+    driver.execute_script('''window.open("https://onlinesequencer.net/forum/chat_frame.php","_blank");''')
+    time.sleep(5)
+
+    driver.switch_to_window(driver.window_handles[1])
+
+    def GETSTAT():
+        bsObj = BeautifulSoup(driver.page_source,'html.parser')
+        container = bsObj.find('div', id='container') #for getting status
+        status = re.findall(r'<div id="status">(.*?)</div>',str(container))
+        return status
+
+    for i in range(1, 20):
+        time.sleep(5)
+        stat = GETSTAT()
+        print(stat)
+
+    driver.close()
+    driver.quit()
+
+while True:
+    time.sleep(.01)
+    RUNMAIN()
